06f3bbb045e00c2be364f6002a5654a9e0dccd38
Delali Kanda <dkanda@nthc.com.gh>
2016-07-21 16:56:33 +0000

    added getEnumField in MCDefaultForm for export data and added notes template

--- src/utils/model_controller/MCDefaultForm.php
+++ src/utils/model_controller/MCDefaultForm.php
@@ -6,7 +6,7 @@ class MCDefaultForm extends Form
     {
         parent::__construct();
         $fields = $model->getFields();
-        
+
         foreach($fields as $name => $field)
         {
             if($field['key'] == 'primary') continue;
@@ -19,60 +19,65 @@ class MCDefaultForm extends Form
             {
                 $element = new ModelField($field["reference"],$field["referenceValue"]);
             }
-            
+
             $element->setRequired($field['required']);
-            
+
             $this->add($element);
         }
-        
+
         $this->addAttribute('style', 'width:50%');
     }
-    
+
     private function getNumericField($field)
     {
         return $this->getStringField($field)->setAsNumeric();
     }
-    
+
     private function getIntegerField($field)
     {
         return $this->getNumericField($field);
     }
-    
+
     private function getDoubleField($field)
     {
         return $this->getNumericField($field);
     }
-    
+
     private function getDateField($field)
     {
         return $this->getElement('DateField', $field);
     }
-    
+
     private function getDateTimeField($field)
     {
         return $this->getElement('DateField', $field);
     }
-    
+
     private function getStringField($field)
     {
         return $this->getElement('TextField', $field);
     }
-    
+
+    private function getEnumField($field)
+    {
+        return $this->getElement('TextField', $field);
+    }
+
     private function getTextField($field)
     {
         return $this->getElement('TextArea', $field);
     }
-    
+
     private function getBooleanField($field)
     {
         return $this->getElement('Checkbox', $field)->setCheckedValue('1');
     }
-    
+
     private function getTimeField($field)
     {
         return $this->getElement('TimeField', $field);
     }
-    
+
     private function getElement($element, $field)
     {
         return Element::create($element, $field['label'], $field['name'], $field['description']);
